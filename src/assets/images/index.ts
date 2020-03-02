b04e809c0d82be0be42aa9ab21dbc3a1d6d86d62
parzivalad <allefwalker73@gmail.com>
2020-03-02 16:37:58 -0300

    Mobile & desktop version of contact session

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,6 +1,13 @@
 export default {
-  project1: require('group-business.jpg'),
-  project2: require('open-mac-with-code.jpg'),
-  project3: require('pear-programming.jpg'),
-  project4: require('half-closed-mac.jpg'),
+  project1: require('./group-business.jpg'),
+  project2: require('./open-mac-with-code.jpg'),
+  project3: require('./pear-programming.jpg'),
+  project4: require('./half-closed-mac.jpg'),
+  logo: {
+    facebook: require('./logo_facebook.png'),
+    instagram: require('./logo_instagram.png'),
+    twitter: require('./logo_twitter.png'),
+    github: require('./logo_github.png'),
+  },
+  telemarketing: require('./telemarketing.svg'),
 };
---
+++ src/components/Contact/styles.tsx
@@ -0,0 +1,88 @@
+import styled from 'styled-components';
+import { colors, metrics, fonts, devices } from '../../styles';
+
+export const Container = styled.div`
+  width: 100%;
+  padding: 20px;
+
+  @media ${devices.fromTablet} {
+    width: 80%;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: ${fonts.titleMobile}px;
+  margin-bottom: 40px;
+
+  @media ${devices.fromTablet} {
+    font-size: ${fonts.titleDesktop}px;
+  }
+`;
+
+export const Subtitle = styled.h2`
+  font-size: ${fonts.subtitleMobile}px;
+  margin-bottom: 40px;
+  width: 80%;
+
+  @media ${devices.fromTablet} {
+    font-size: ${fonts.subtitleDesktop}px;
+  }
+`;
+
+export const Description = styled.p`
+  font-size: 18px;
+  margin-bottom: 20px;
+  width: 80%;
+
+  @media ${devices.fromTablet} {
+    font-size: ${fonts.bigger}px;
+  }
+`;
+
+export const CompanyButtonsContainer = styled.div`
+  margin-bottom: 20px;
+
+  @media ${devices.fromTablet} {
+    margin-bottom: 40px;
+  }
+`;
+
+export const CompanyButton = styled.button`
+  background-color: white;
+  border-style: none;
+  cursor: pointer;
+`;
+
+export const CompanyLogo = styled.img`
+  margin-right: 10px;
+  width: 25px;
+  height: 25px;
+
+  @media ${devices.fromTablet} {
+    margin-right: 20px;
+    width: 50px;
+    height: 50px;
+  }
+`;
+
+export const Email = styled.p`
+  font-weight: bold;
+  font-size: ${fonts.subtitleMobile}px;
+  
+  @media ${devices.fromTablet} {
+    font-size: ${fonts.subtitleDesktop}px;
+  }
+`;
+
+export const TelemarketingContainer = styled.div`
+  width: 100%;
+`;
+
+export const TelemarketingImage = styled.img`
+  width: 100%;
+
+  @media ${devices.fromTablet} {
+    width: 40%;
+    float: right;
+  }
+`;
