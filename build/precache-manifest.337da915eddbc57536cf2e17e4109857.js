9fc728e8cb3323dfca0901798dda7733e4e93206
parzivalad <allefwalker73@gmail.com>
2020-03-03 11:10:56 -0300

    Adding build folder

---
+++ build/precache-manifest.337da915eddbc57536cf2e17e4109857.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0220b4ae7ff57b76f636f4e2059c6e31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4489f0a057556113915b",
+    "url": "/static/js/2.e69225e2.chunk.js"
+  },
+  {
+    "revision": "4f4c9d0d65b6661305a9f768c2e5308a",
+    "url": "/static/js/2.e69225e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a01dd3d425ce7a668eb4",
+    "url": "/static/js/main.28b64ce7.chunk.js"
+  },
+  {
+    "revision": "9af90a3bdb44f1b1746a",
+    "url": "/static/js/runtime-main.fb18d836.js"
+  },
+  {
+    "revision": "10c932e800a91e891b31392a007e9c67",
+    "url": "/static/media/allef.10c932e8.png"
+  },
+  {
+    "revision": "d43e23774756079dd2b0cb30b690e86e",
+    "url": "/static/media/flavio.d43e2377.jpeg"
+  },
+  {
+    "revision": "7e3b10a4826d2881494adcb3770ad783",
+    "url": "/static/media/group_business.7e3b10a4.jpg"
+  },
+  {
+    "revision": "4c9e6d262894be6499130e494ddc62e6",
+    "url": "/static/media/half-closed-mac.4c9e6d26.jpg"
+  },
+  {
+    "revision": "837025776f78152202078319d911dcb9",
+    "url": "/static/media/homeFooter.83702577.svg"
+  },
+  {
+    "revision": "2a6a4c5a018fbdfe7a492ff878eee7cb",
+    "url": "/static/media/logo_android_studio.2a6a4c5a.png"
+  },
+  {
+    "revision": "aa924925d08de8aba453d7fbb028b1bc",
+    "url": "/static/media/logo_aws.aa924925.png"
+  },
+  {
+    "revision": "c1554359d9471f39472a588dc66d89ad",
+    "url": "/static/media/logo_facebook.c1554359.png"
+  },
+  {
+    "revision": "03f40b67b63d9c1c1a5a792109bbc699",
+    "url": "/static/media/logo_instagram.03f40b67.png"
+  },
+  {
+    "revision": "4eb1819dc863ef386e09d86e9f482a9f",
+    "url": "/static/media/logo_java.4eb1819d.png"
+  },
+  {
+    "revision": "3503c59a33333db03cc26e95841c8d28",
+    "url": "/static/media/logo_node.3503c59a.jpg"
+  },
+  {
+    "revision": "7ddd902a232bb9f157b4678f1f70fbec",
+    "url": "/static/media/logo_php.7ddd902a.png"
+  },
+  {
+    "revision": "4af927e2fc1041447294de7e5cbcd446",
+    "url": "/static/media/logo_react.4af927e2.png"
+  },
+  {
+    "revision": "91c116d019ccc8c7279dbed729d54826",
+    "url": "/static/media/logo_twitter.91c116d0.png"
+  },
+  {
+    "revision": "fe1b26e3967db294a01460124b7ffe58",
+    "url": "/static/media/man_running_with_coin.fe1b26e3.svg"
+  },
+  {
+    "revision": "e9676f67d07c4435ed67fbab9b32ad15",
+    "url": "/static/media/man_sitting_on_coins.e9676f67.svg"
+  },
+  {
+    "revision": "466cd745849ae09d494a29f640da1b7a",
+    "url": "/static/media/monitor.466cd745.svg"
+  },
+  {
+    "revision": "15c1f6aa5635936293964e105b49feb4",
+    "url": "/static/media/open-mac-with-code.15c1f6aa.jpg"
+  },
+  {
+    "revision": "18b46b3e9fe20e5d255a708c7b31bec5",
+    "url": "/static/media/pear-programming.18b46b3e.jpg"
+  },
+  {
+    "revision": "7db0434351a14c5356045cf640b48c2d",
+    "url": "/static/media/telemarketing.7db04343.svg"
+  }
+]);
